fbd4728c54d498bd032317eb4c48fbd55b84d821
ProgramandoComAndre <andrerafael112@gmail.com>
2024-02-13 14:31:17 +0000

    Created a student list with properties. Created for debugging propTypes to validate the values passed on each student then added a placeholder value with defaultProps

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Student from "./components/Students";
 
 function App() {
   return (
-    <h1>Placeholder</h1>
+    <>
+      <Student name="Andre" age={30} isStudent={true}></Student>
+      <Student name="Paulo" age={45} isStudent={false}></Student>
+      <Student name="Goncalo" age={23} isStudent={false}></Student>
+      <Student></Student>
+    </>
   );
 }
 
